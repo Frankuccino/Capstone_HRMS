78813988f0e7ee63da65079b3608537c9920f1eb
Frankuccino <frankabad189@gmail.com>
2023-08-12 00:39:13 +0800

    added chartJS data through controller

--- server/routes/dashboardRouter.js
+++ server/routes/dashboardRouter.js
@@ -4,6 +4,6 @@ const { viewDashboard, test } = require('../controller/dashboardController');
 const {isLoggedIn} = require('../../middlewares');
 
 router.get('/', isLoggedIn, viewDashboard);
-router.get('/test', test)
+
 
 module.exports = router;
